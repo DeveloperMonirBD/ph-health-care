1a2a2361e83b26aafdcbf3019c67d5af1ea62952
Monir <mrmonir0558@gmail.com>
2025-11-15 14:44:07 +0600

    navbar design almost completed

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import PublicNavbar from "@/components/modules/shared/PublicNavbar";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <>
+                <PublicNavbar />
+                {children}
+            </>
+        </div>
+    );
+};
+
+export default CommonLayout;
