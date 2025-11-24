64502d346c9198c7bed5bef1c19128f50790845d
Monir <mrmonir0558@gmail.com>
2025-11-24 23:20:39 +0600

    Protecting and Authorizing the pages with Main Proxy Function

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,136 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+type UserRole = 'ADMIN' | 'DOCTOR' | 'PATIENT';
+
+// exat: ["/my-profile", "settings"]
+// patterns: [/^\/dashboard, /^\/patient], // Routes starting with /dashboard/* /patient/*
+type RouteConfig = {
+    exact: string[];
+    patterns: RegExp[];
+};
+
+const authRoutes = ['/login', '/register', '/forgot-password', '/reset-password'];
+
+const commonProtectedRoutes: RouteConfig = {
+    exact: ['/my-profile', '/settings'],
+    patterns: [] // [/password/change-password, /password/reset-password => /password/*]
+};
+
+const doctorProtectedRoutes: RouteConfig = {
+    patterns: [/^\/doctor/], // Routes starting with  /doctor*, /assitants, /appointments/*
+    exact: [] // "/assistants"
+};
+
+const adminProtectedRoutes: RouteConfig = {
+    patterns: [/^\/admin/], // Routes starting with /admin/*
+    exact: [] // "/admins"
+};
+
+const patientProtectedRoutes: RouteConfig = {
+    patterns: [/^\/dashboard/], // Routes starting with /dashboard/*
+    exact: [] // "/dashboard"
+};
+
+const isAuthRoute = (pathname: string) => {
+    return authRoutes.some((route: string) => route === pathname);
+};
+
+const isRouteMatches = (pathname: string, routes: RouteConfig): boolean => {
+    if (routes.exact.includes(pathname)) {
+        return true;
+    }
+
+    return routes.patterns.some((pattern: RegExp) => pattern.test(pathname));
+};
+
+const getRouteOwner = (pathname: string): 'ADMIN' | 'DOCTOR' | 'PATIENT' | 'COMMON' | null => {
+    if (isRouteMatches(pathname, adminProtectedRoutes)) {
+        return 'ADMIN';
+    }
+    if (isRouteMatches(pathname, doctorProtectedRoutes)) {
+        return 'DOCTOR';
+    }
+    if (isRouteMatches(pathname, patientProtectedRoutes)) {
+        return 'PATIENT';
+    }
+    if (isRouteMatches(pathname, commonProtectedRoutes)) {
+        return 'COMMON';
+    }
+    return null;
+};
+
+const getDefaultDashboardRoute = (role: UserRole): string => {
+    if (role === 'ADMIN') {
+        return '/admin/dashboard';
+    }
+    if (role === 'DOCTOR') {
+        return '/doctor/dashboard';
+    }
+    if (role === 'PATIENT') {
+        return '/dashboard';
+    }
+    return '/';
+};
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-    console.log("pathname", request.nextUrl.pathname);
+export async function proxy(request: NextRequest) {
+    const cookieStore = await cookies();
+    const pathname = request.nextUrl.pathname;
+
+    const accessToken = request.cookies.get('accessToken')?.value || null;
+
+    let userRole: UserRole | null = null;
+
+    if (accessToken) {
+        const verifiedToken: string | jwt.JwtPayload = jwt.verify(accessToken, process.env.JWT_SECRET as string);
+
+        if (typeof verifiedToken === 'string') {
+            cookieStore.delete('accessToken');
+            cookieStore.delete('refreshToken');
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+
+        userRole = verifiedToken.role;
+    }
+
+    const routerOwner = getRouteOwner(pathname);
+    const isAuth = isAuthRoute(pathname);
+
+    // Rule 1 : User is Logged in and trying to access auth route. Redirect to default dashboard
+    if (accessToken && isAuth) {
+        return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url));
+    }
+
+    // Rule 2: User is trying to access open public route
+    if (routerOwner === null) {
+        return NextResponse.next();
+    }
+
+    // Rule 1 & 2 for open public routes and auth routes
+
+    if (!accessToken) {
+        const loginUrl = new URL("/login", request.url);
+        loginUrl.searchParams.set("redirect", pathname)
+        return NextResponse.redirect(loginUrl);
+    }
+
+    // Rule 3: User is trying to access common protected route
+    if (routerOwner === 'COMMON') {
+        return NextResponse.next();
+    }
+
+    // Rule 4: User is trying to access role based protected route
+    if (routerOwner === 'ADMIN' || routerOwner === 'DOCTOR' || routerOwner === 'PATIENT') {
+        if (userRole !== routerOwner) {
+            return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url));
+        }
+        return NextResponse.next();
+    }
+
+    console.log(userRole);
+    // console.log("pathname", request.nextUrl.pathname);
     return NextResponse.next();
 }
 
