8a4a817dd2110e97c25d571d6cb8ade9bdb7d9bf
Monir <mrmonir0558@gmail.com>
2025-12-06 14:53:59 +0600

    Created Dynamic Rolebased Navigation Items For Sidebar

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -2,13 +2,19 @@
 import { UserInfo } from '@/services/types/user.interface';
 import DashboardNavbarContent from './DashboardNavbarContent';
 import { getUserInfo } from '@/services/auth/getUserInfo';
+import { NavSection } from '@/services/types/dashboard.interface';
+import { getNavItemsByRole } from '@/lib/navItems.config';
+import { getDefaultDashboardRoute } from '@/lib/auth-utils';
 
 const DashboardNavbar = async () => {
     const userInfo = (await getUserInfo()) as UserInfo;
+    
+    const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role);
 
     return (
         <div>
-            <DashboardNavbarContent userInfo={userInfo} />
+            <DashboardNavbarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome} />
         </div>
     );
 };
--- src/components/modules/Dashboard/DashboardNavbarContent.tsx
+++ src/components/modules/Dashboard/DashboardNavbarContent.tsx
@@ -3,17 +3,51 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { UserInfo } from '@/services/types/user.interface';
-import { Bell, Search } from 'lucide-react';
+import { Bell, Menu, Search } from 'lucide-react';
 import UserDropdown from './UserDropdown';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import DashboardMobileSidebar from './DashboardMobileSidebar';
+import { NavSection } from '@/services/types/dashboard.interface';
+import { useEffect, useState } from 'react';
 
 interface DashboardNavbarContentProps {
     userInfo: UserInfo;
+    navItems?: NavSection[];
+    dashboardHome?: string;
 }
 
-const DashboardNavbarContent = ({ userInfo }: DashboardNavbarContentProps) => {
+const DashboardNavbarContent = ({ userInfo, navItems, dashboardHome }: DashboardNavbarContentProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => { 
+        const checkSmallerScreen = () => {
+            setIsMobile(window.innerWidth < 768);
+        }
+
+        checkSmallerScreen();
+        window.addEventListener('resize', checkSmallerScreen);
+
+        return () => {
+            window.removeEventListener('resize', checkSmallerScreen);
+        }       
+    },[])
+
     return (
         <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
             <div className="flex h-16 items-center justify-between gap-4 md:px-6">
+                {/* mobile menu sheet */}
+                <Sheet open={isMobile && isOpen} onOpenChange={setIsOpen}>
+                    <SheetTrigger asChild className='md:hidden'>
+                        <Button variant="outline" size="icon">
+                            <Menu className="h-5 w-5" />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left" className="w-64 p-0 md:hidden">
+                        <DashboardMobileSidebar userInfo={userInfo} navItems={navItems || []} dashboardHome={dashboardHome || ''} />
+                    </SheetContent>
+                </Sheet>
+
                 {/* search bar */}
                 <div className="flex-1">
                     <div className="relative">
